33c738123c39ecac8ea1ad563b9fe0b78b5810d4
ertellucas98-dev <ertellucas98@gmail.com>
2026-03-31 15:46:16 -0300

    Adicionando componente Greeting ao projeto

---
+++ src/Greeting.tsx
@@ -0,0 +1,13 @@
+
+
+function Greeting () {
+    return (
+        <div style={{ padding: '20px', textAlign: 'center' }}>
+            <h1>Olá, bem-vindo ao React! 👋</h1>
+            <p>Este é seu primeiro componente React.</p>
+        </div>
+    );
+};
+
+
+export default Greeting;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import Greeting from './Greeting.tsx'
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+   <Greeting />
   </StrictMode>,
 )
